237d7a1a80a95ae726d85dfc9f17d396ee1a505a
Troley <troleyhs@gmail.com>
2024-03-19 19:21:28 +0100

    Project app mainloop

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import pandas
 
-db = pandas.read_csv("hotels.csv")
+df = pandas.read_csv("hotels.csv")
 
 
 class Hotel:
+    def __init__(self, id):
+        pass
+
     def book(self):
+        pass
+
+    def available(self):
         pass
 
 
 class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        pass
+
     def generate(self):
         pass
+
+
+print(df)
+id_hotel = input("Enter the id of the hotel: ")
+hotel = Hotel(id)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("Hotel is not free.")
